a402324ce877d4e206a755518a1557568e223e81
avihu sason <aviho004@gmail.com>
2022-05-02 23:11:13 +0300

    Update problamaticCostumers.sql
    rm nonTrivialQuerys

--- trivialQuerys/problamaticCostumers.sql
+++ trivialQuerys/problamaticCostumers.sql
@@ -19,8 +19,3 @@ union
 select * from ASASON.BLACKLIST
 
 
-# get id,  FIRST_NAME,  LAST_NAME,  reason#
-select t1.*,ASASON.COSTUMERSERVICE.reason from (select c.id, c.FIRST_NAME, c.LAST_NAME
-from gruenspe.costumers c, ASASON.BLACKLIST bl
- where c.id = bl.costumerid ) t1
- LEFT JOIN  ASASON.COSTUMERSERVICE on COSTUMERSERVICE.costumerid = t1.id
