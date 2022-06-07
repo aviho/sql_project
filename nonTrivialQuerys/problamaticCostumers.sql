1f90efd2673e8cb424c6dbcf32a23b7c600cac11
Yehoshua Gruenspecht <72933775+Yehoshua18@users.noreply.github.com>
2022-06-07 10:46:30 +0300

    Delete problamaticCostumers.sql

--- nonTrivialQuerys/problamaticCostumers.sql
+++
@@ -1,6 +0,0 @@
-
-# get id,  FIRST_NAME,  LAST_NAME,  reason#
-select t1.*,ASASON.COSTUMERSERVICE.reason from (select c.id, c.FIRST_NAME, c.LAST_NAME
-from gruenspe.costumers c, ASASON.BLACKLIST bl
- where c.id = bl.costumerid ) t1
- LEFT JOIN  ASASON.COSTUMERSERVICE on COSTUMERSERVICE.costumerid = t1.id
